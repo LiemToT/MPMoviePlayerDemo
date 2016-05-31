fbe640606da6437f8ac636a06ae97b02b8a81480
linyi101 <804121494@qq.com>
2016-05-31 12:27:19 +0800

    Complete mpPlayer

--- MPMoviePlayerDemo/ViewController.swift
+++ MPMoviePlayerDemo/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    @IBAction func mpPlayer(sender: AnyObject) {
+        let urlString = "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4"
+        let player = MPMoviePlayerViewController(contentURL: NSURL(string: urlString))
+        self.presentMoviePlayerViewControllerAnimated(player!)
+    }
 
+    @IBAction func avPlayer(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
